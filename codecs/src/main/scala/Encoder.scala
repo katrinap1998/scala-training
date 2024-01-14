9124858a1883aa2767e1e5460360048363afa867
Katrina Petrevice <katrinapetrevice1998@gmail.com>
2024-01-14 20:30:06 +0000

    Structure Json Encoder

--- codecs/src/main/scala/Encoder.scala
+++ codecs/src/main/scala/Encoder.scala
@@ -1,1 +1,22 @@
 package com.jpmc.codecs
+
+import com.jpmc.json.Json
+
+sealed trait JsonEncoder[A] {
+  def encode(value: A): Json
+}
+
+object JsonEncoder {
+
+  def apply[A] = ???
+  def from[A] = ???
+
+  implicit val intEncoder = ???
+  implicit val stringEncoder = ???
+  implicit val booleanEncoder = ???
+
+  implicit def arrayEncoder = ???
+  implicit def objectEncoder = ???
+  implicit def optionEncoder = ???
+  implicit def eitherEncoder = ???
+}
