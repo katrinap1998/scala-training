2d7d6d20ea24651240bdb23deb3c3bba2870c5f4
Katrina Petrevice <katrinapetrevice1998@gmail.com>
2024-01-14 20:17:28 +0000

    Define basic Json structure

--- json/src/main/scala/Json.scala
+++ json/src/main/scala/Json.scala
@@ -1,1 +1,16 @@
 package com.jpmc.json
+
+
+sealed trait Json extends Product with Serializable
+
+object Json {
+
+  case object JsonNull extends Json
+  sealed trait JsonNumber extends Json
+  final case class JsonString(value: String) extends Json
+  final case class JsonInt(value: Int) extends JsonNumber
+  final case class JsonBoolean(value: Boolean) extends Json
+  final case class JsonArray(value: List[Json]) extends Json
+  final case class JsonObj(value: Map[String, Json]) extends Json
+
+}
